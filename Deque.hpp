0f6ffba55d60bf6bc0056f41f69748367d186571
Dylan Macejko <dmacejk1@cs.binghamton.edu>
2021-09-10 13:48:53 -0400

    initializing structs with int first

---
+++ Deque.hpp
@@ -0,0 +1,55 @@
+#ifndef DEQUE_HPP
+#define DEQUE_HPP
+
+
+void push_b(struct Deque_int * deque, int value){
+	(*deque).container = realloc(container, sizeof(container) + sizeof(int));
+	(*deque).container[capacity] = value;
+	(*deque).capacity = (*deque).capacity + 1
+}
+
+void push_f(struct Deque_int * deque, int value){
+	//include the reallocation of elements here, figure out circular dynamic array allocation
+	(*deque).container[0] = value;
+	(*deque).capacity = (*deque).capacity + 1;
+}
+
+int container_size(struct Deque * deque){
+	return (*deque).capacity;
+}
+
+void pop_b(struct Deque * deque){
+
+}
+
+void pop_f(struct Deque * deque){
+
+}
+
+
+void Deque_int_ctor(struct Deque_int *, bool (* f)(const int, const int){
+	container = malloc(sizeof(int));
+	type_name = "int";
+	capacity = 1;
+	push_back = &push_b;
+	push_front = &push_f;
+	size = &container_size;
+	pop_back = &pop_b;
+	pop_front = &pop_f;
+}
+
+
+struct Deque_int{	
+	int* container;
+	char * type_name;
+	void (*push_back)(struct Deque_int *, int);
+	void (*push_front)(struct Deque_int *, int);
+	int (*size)(struct Deque_int *);
+	void (*pop_back)(struct Deque_int *);
+	void (*pop_front)(struct Deque_int *);
+}
+
+#define Deque_DEFINE
+
+
+#endif
